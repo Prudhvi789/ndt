a24e5c3c9256d43cb907bf745936efc56210aeab
Prudhvi B <prudhvib460@gmail.com>
2021-02-22 17:21:56 +0530

    second commit

--- routes/testAPI.js
+++ routes/testAPI.js
@@ -1,8 +1,24 @@
 var express = require("express");
 var router = express.Router();
+const mongodb = require("mongodb");
 
-router.get("/", function(req, res, next) {
-    res.send("API is working properly");
-});
+//const port = process.env.PORT || 5000
+
+const mongoClient = mongodb.MongoClient
+
+const uri = "mongodb+srv://Prudhvi:prudhvidbs@cluster0.fztff.mongodb.net/<dbname>?retryWrites=true&w=majority";
+
+router.get("/",async (req,res)=>{
+    try{
+        let client = await mongoClient.connect(uri);
+        let db = client.db("zen");
+        let students = await db.collection("products").find().toArray();
+        client.close();
+        res.status(200).json(students);
+    }
+    catch(err){
+        res.status(400).json({ message: err.message })
+    }
+})
 
 module.exports = router;
